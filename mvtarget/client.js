9fbaf077e4e33693eb3f2a3360e314014700118f
ausmo42 <ausmo42@gmail.com>
2019-03-30 16:32:30 -0400

    deleted target folder for reals this time.

--- mvtarget/client.js
+++
@@ -1,8 +0,0 @@
-
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.use(express.static('mvproject/www'))
-
-app.listen(port, () => console.log(`oreHero client launched on port ${port}!`))
